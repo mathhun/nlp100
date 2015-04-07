3b13723f9c1b0f260891ba86f1e9f26bd2824af6
mathhun <achemer@gmail.com>
2015-04-07 22:15:06 +0900

    NLP100, 15th day, 53

--- nlp053.py
+++ nlp053.py
@@ -7,9 +7,11 @@ Stanford Core NLPを用い，入力テキストの解析結果をXML形式で得
 """
 
 import corenlp
+from optparse import OptionParser
+import re
 import xmltodict
 
-def main():
+def tokenize():
     corenlp_dir = "./lib/stanford-corenlp-full-2015-01-29/"
     #parser = corenlp.StanfordCoreNLP(corenlp_path=corenlp_dir)
     parsed = corenlp.batch_parse('tmp/', corenlp_path=corenlp_dir, raw_output=True)
@@ -17,6 +19,25 @@ def main():
     for p in parsed:
         text = xmltodict.unparse(p, pretty=True)
         print(text)
+
+def show_words(path):
+    with open(path) as f:
+        for line in f:
+            m = re.search('<word>(.*)</word>', line)
+            if m:
+                print(m.group(1))
+
+def main():
+    parser = OptionParser()
+    parser.add_option("-t", "--tokenize", dest="tokenize", default=False)
+    parser.add_option("-w", "--word",     dest="word",     default=None)
+    (options, args) = parser.parse_args()
+
+    if options.tokenize:
+        tokenize()
+
+    if options.word is not None:
+        show_words(options.word)
 
 if __name__ == '__main__':
     main()
